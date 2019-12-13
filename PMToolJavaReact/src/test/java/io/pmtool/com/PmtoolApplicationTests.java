087acba201fc5ff7b9ed53f5273484a43d429484
Kenny <kleung@email.com>
2019-12-13 00:04:13 -0500

    first commit

---
+++ PMToolJavaReact/src/test/java/io/pmtool/com/PmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pmtool.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
